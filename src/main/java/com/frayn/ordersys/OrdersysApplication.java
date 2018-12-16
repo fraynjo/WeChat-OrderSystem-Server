c9d3ab67ea7ae429bace23fd5d8c11f50008d972
Frayn Zhao <fraynzhao@FrayndeMacBook-Air.local>
2018-12-16 15:31:18 +0800

    ordersys init

---
+++ src/main/java/com/frayn/ordersys/OrdersysApplication.java
@@ -0,0 +1,14 @@
+package com.frayn.ordersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersysApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/frayn/ordersys/repository/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.frayn.ordersys.repository;
+
+import com.frayn.ordersys.dataobject.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
+
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
+}
